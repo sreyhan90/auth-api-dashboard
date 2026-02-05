da933ba127b5473e1a501c407765e590ea662772
Semir Reyhan <semir.reyhan1990@gmail.com>
2026-02-05 13:27:19 +0300

    feat: setup login form with formik

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,14 +1,44 @@
 import "../CSS/Login.css";
+import { Formik } from "formik";
 
 function LoginPage() {
   return (
     <div className="login-page">
       <div className="login-card">
         <span className="login-title">Login</span>
+        <Formik
+          initialValues={{
+            username: "",
+            password: "",
+          }}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+        >
+          {({ values, handleSubmit, handleChange }) => (
+            <form onSubmit={handleSubmit}>
+              <input
+                className="login-input"
+                name="username"
+                type="text"
+                placeholder="username"
+                value={values.username}
+                onChange={handleChange}
+              />
 
-        <input className="login-input" type="text" placeholder="Username" />
+              <input
+                className="login-input"
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={values.password}
+                onChange={handleChange}
+              />
 
-        <input className="login-input" type="password" placeholder="Password" />
+              <button className="login-btn">Login</button>
+            </form>
+          )}
+        </Formik>
 
         <button className="login-btn">Login</button>
       </div>
