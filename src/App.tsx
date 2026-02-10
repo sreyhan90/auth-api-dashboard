633e9ec0ff8c61f611418b269a43b9aee35a3220
Semir Reyhan <semir.reyhan1990@gmail.com>
2026-02-10 13:12:18 +0300

    feat: redirect authenticated users away from login

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import LoginPage from "../src/Pages/LoginPage";
 import DashboardHomePage from "../src/Pages/DashboardHomePage";
 import ProtectedRoute from "./Routes/ProtectedRoute";
+import { useAppSelector } from "../src/app/hooks";
 
 function App() {
+  const token = useAppSelector((s) => s.auth.token);
   return (
     <>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/Login" element={<LoginPage />} />
+        <Route
+          path="/Login"
+          element={token ? <Navigate to="/dashboard" replace /> : <LoginPage />}
+        />
         <Route
           path="/Dashboard"
           element={
