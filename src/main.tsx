b47d454821a5afb2acffa3a02a5641c7949017d5
Semir Reyhan <semir.reyhan1990@gmail.com>
2026-02-12 18:15:47 +0300

    feat: persist user info in localStorage for session recovery

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -6,11 +5,9 @@ import App from "./App";
 import { store } from "./app/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
